1d913d863c09ec9e5815ee6abbfceeefbc44733c
Chhatrapal Maurya <chhatrapaltesting@gmail.com>
2026-08-19 16:34:28 +0530

    commit 3

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 0,  // iska mtlb - ki fail test ko kitni bar runkrna hai - 0 ki  kr diya to - 1 war rerun hoga
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
